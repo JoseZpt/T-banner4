a4c2c253d39a7f803874234de37b4d814cb15698
JIN26 <Angelmantilla26@gmail.com>
2021-02-12 02:04:41 -0500

    rename banner.sh

--- banner.sh
+++ banner.sh
@@ -42,10 +42,10 @@ case $(command -v figlet && command -v python2 && command -v setterm && command 
   "")
     case $jk in
       0)
-      ;;
+        ;;
       *)
-      echo -e "\n$r[-]$b1 banner:$r No Hay Conexion A Internet.\n$b"
-      exit
+        -e "\n$r[-]$b1 banner:$r No Hay Conexion A Internet.\n$b"
+        exit
     esac
   clear
   apt install -y figlet
@@ -60,6 +60,7 @@ case $(command -v figlet && command -v python2 && command -v setterm && command 
   source 'banner'
   exit
 esac
+
 #Listado y seleccion del banner
 banner() {
   clear
@@ -83,64 +84,65 @@ banner() {
   $A[${b1}12$A]${b1} Diablo
   $A[${b1}13$A]${b1} Pulpo
   $A[${b1}14$A]${b1} Calavera
-  $A[${b1}15$A]${b1} Banner Personal\n"
-  printf " $A[$b1+$A]$v banner:$b "
+  $A[${b1}15$A]${b1} Banner Personal"
+  echo -e "  $A[$b1+$A]$v banner:$b"
   read banner
   case $banner in
     01|1)
       banner=$(cat $PWD/.Banners/hacker)
-    ;;
+      ;;
     02|2)
       banner=$(cat $PWD/.Banners/hacker2)
-    ;;
+      ;;
     03|3)
       banner=$(cat $PWD/.Banners/ruby)
-    ;;
+      ;;
     04|4)
       banner=$(cat $PWD/.Banners/bash)
-    ;;
+      ;;
     05|5)
       banner=$(cat $PWD/.Banners/craneo)
-    ;;
+      ;;
     06|6)
       banner=$(cat $PWD/.Banners/craneo2)
-    ;;
+      ;;
     07|7)
       banner=$(cat $PWD/.Banners/ghost)
-    ;;
+      ;;
     08|8)
       banner=$(cat $PWD/.Banners/scorpion)
-    ;;
+      ;;
     09|9)
       banner=$(cat $PWD/.Banners/cobra)
-    ;;
+      ;;
     10)
       banner=$(cat $PWD/.Banners/arma)
-    ;;
+      ;;
     11)
       banner=$(cat $PWD/.Banners/kali)
-    ;;
+      ;;
     12)
       banner=$(cat $PWD/.Banners/diablo)
-    ;;
+      ;;
     13)
       banner=$(cat $PWD/.Banners/pulpo)
-    ;;
+      ;;
     14)
       banner=$(cat $PWD/.Banners/calavera)
-    ;;
+      ;;
     15)
       echo -e "${v}Pega el banner y cuando termines das 'ESC:x' "
       read -p "Presiona Enter Para Continuar: "
       nvim .Banners/nuevo
       banner=$(cat $PWD/.Banners/nuevo)
-    ;;
+      ;;
     *)
       echo -e " $r[-]$b1 Banner invalido..!$b"
       sleep 0.5
       banner
   esac
 }
+
 #Listado y sellecion del color
 color() {
   clear
@@ -165,54 +167,54 @@ color() {
   $A[${b1}13$A]${a} Amarillo
   $A[${b1}14$A]${a1} Amarillo2
   $A[${b1}15$A]${g} Gris"
-  printf " $A[$b1+$A]$v banner:$b "
+  echo -e "  $A[$b1+$A]$v banner:$b "
   read color
   case $color in
     01|1)
       Color=$b
-    ;;
+      ;;
     02|2)
       Color=$b1
-    ;;
+      ;;
     03|3)
       Color=$r
-    ;;
+      ;;
     04|4)
       Color=$r1
-    ;;
+      ;;
     05|5)
       Color=$A
-    ;;
+      ;;
     06|6)
       Color=$A1
-    ;;
+      ;;
     07|7)
       Color=$ac
-    ;;
+      ;;
     08|8)
       Color=$ac1
-    ;;
+      ;;
     09|9)
       Color=$v
-    ;;
+      ;;
     10)
       Color=$v1
-    ;;
+      ;;
     11)
       Color=$m
-    ;;
+      ;;
     12)
       Color=$m1
-    ;;
+      ;;
     13)
       Color=$a
-    ;;
+      ;;
     14)
       Color=$a1
-    ;;
+      ;;
     15)
       Color=$g
-    ;;
+      ;;
     *)
       echo -e " $r[-]$b1 Color invalido..!$b"
       sleep 0.5
@@ -220,6 +222,7 @@ color() {
   esac
 }
 
+#veces que se repita
 titilo() {
   clear
   setterm -foreground green;
@@ -229,23 +232,31 @@ titilo() {
   Cuantas veces quiere que titile: "
   read veces
 }
+
 banner
 color
 titilo
+
 if [ -e $PREFIX/etc/motd ]; then
-  rm $PREFIX/etc/motd
+rm $PREFIX/etc/motd
 fi
 if [ -e $PREFIX/etc/bash.bashrc ]; then
-  rm $PREFIX/etc/bash.bashrc
-  echo 'if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
-          command_not_found_handle() {
-                  /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
-          }
+rm $PREFIX/etc/bash.bashrc
+echo '
+if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+  command_not_found_handle() {
+    /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+  }
 fi
-  logo
 
 PS1='Jin$ '
+setterm -foreground red
+figlet -cf big JIN26";
 python2 Banner
+setterm -foreground green
+figlet -cf slant $OSTYPE";
+figlet -cf big "${local:0:11}:8080" ||cowsay sorry
+
 '>$PREFIX/etc/bash.bashrc
 
 rm Banner &>> /dev/null
@@ -254,54 +265,54 @@ echo "
 #coding=utf-8
 import os,sys,time
 def maung(n):
-  for m in n + '\n':
-    sys.stdout.write(m)
-    sys.stdout.flush()
-    time.sleep(0.001)
-    banner = """
-    $Color
-    $banner
-    \033[0m"""\n
-    os.system("clear")
-    maung(banner)" > Banner
-
-case $veces in
-"")
-veces=40
-nn=1
-while [ $nn -le $veces ]; do
-nn=$(($nn + 1))
-echo 'os.system("clear")' >> Banner
-echo "print (banner)" >> Banner
-done
-echo "print" >> Banner
-sleep 1
-;;
-*)
-nn=1
-while [ $nn -le $veces ]; do
-nn=$(($nn + 1))
-echo 'os.system("clear")' >> Banner
-echo "print (banner)" >> Banner
-done
-echo "print" >> Banner
-sleep 1
-esac
-if [ -e .Banner ]; then
-rm .Banner
-fi
-b1
-echo -ne "${v}Ver Banner(y/n):$b "
-read yaa
-case $yaa in
-y|Y)
-python2 Banner
-;;
-n|N)
-echo -e "$b"
+for m in n + '\n':
+  sys.stdout.write(m)
+  sys.stdout.flush()
+  time.sleep(0.001)
+  banner = """
+  $Color
+  $banner
+  $b"""\n
+  os.system("clear")
+  maung(banner)" > Banner
+  case $veces in
+    "")
+      veces=40
+      i=1
+      while [ $i -le $veces ]; do
+        nn=$(($i + 1))
+        echo 'os.system("clear")' >> Banner
+        echo "print (banner)" >> Banner
+      done
+      echo "print" >> Banner
+      sleep 1
+      ;;
+    *)
+      i=1
+      while [ $i -le $veces ]; do
+        i=$(($i + 1))
+        echo 'os.system("clear")' >> Banner
+        echo "print (banner)" >> Banner
+      done
+      echo "print" >> Banner
+      sleep 1
+  esac
+  if [ -e .Banner ]; then
+  rm .Banner
+  fi
+  autor
+  echo -ne "${v}Ver Banner(y/n):$b "
+  read yaa
+  case $yaa in
+    y|Y)
+      python2 Banner
+    ;;
+    n|N)
+      echo -e "$b"
+      exit
+    ;;
+    *)
+      echo -e "$b"
+      exit
+  esac
 exit
-;;
-*)
-echo -e "$b"
-exit
-esac
