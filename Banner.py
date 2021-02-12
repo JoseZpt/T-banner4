fdb6797cc70d96c5efb6675c6d616858040c7de3
JIN26 <Angelmantilla26@gmail.com>
2021-02-12 03:09:38 -0500

    Update Banner.py

---
+++ Banner.py
@@ -0,0 +1,119 @@
+#!/bin/python2
+#coding=utf-8
+import os,sys,time
+def maung(n):
+  for m in n + '\n':
+    sys.stdout.write(m)
+    sys.stdout.flush()
+    time.sleep(0.001)
+banner = """
+[0m[1;31m
+                                        :`  .
+                                       -s- -o.
+                                       -do./d+`
+                           .`-+:-`:o:` -hd+/dh:
+                       .+++++mmmdhNmdy++dmhshds-
+                    ..-oNNNNNNNNNNNNNNmmmmmdmdds-
+                  +hdmmNNNNNNNmmmmmNNNmmmmmmdddds:`
+                -+mNNNNNdy+::-----:+shmmmmmddddddy:
+               .sdNNNNNd+-           `-ohmmmddddhddo`
+              `:dNNNNh-..               .+hdddddhshms.
+             `:dmmNNN:..                  .sddddddhmmy-
+             .osmNmNm-..                    :sdddddmmmo'
+             `:NNNNNs..                      ./ydddmmh-`
+               sdmNNNNs-                         -odmmmms-
+              `-.:mNNNNd+.                         .ommmmmh-
+                 `ydhmNNNds:.                       +mmmmh.
+                   .`:odmNNNds/.`                    -mmh+``
+                       `.-+ydmNNmds+-`                -:.`
+                         ``-/oydmNNmyo-
+                                `.:oymmmh+.
+                                    `-ohmmo.
+                                         .odd:
+                                           `od:
+                                             /d`
+                                              y.
+                                             .`[0m"""
+os.system("clear")
+maung(banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+os.system("clear")
+print (banner)
+print
